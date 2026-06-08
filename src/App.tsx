685c63b90e4c32c088df57d35c38faf2e66ccb8b
Italo Herbert Siqueira Gabriel <italoherbert@outlook.com>
2026-06-08 16:52:55 -0300

    Consertado pequeno detalhe no resumo do jogo de xadrez

--- src/App.tsx
+++ src/App.tsx
@@ -143,7 +143,7 @@ function App() {
         <p>O Italo Xadrez é um jogo de xadrez produzido em C++ com biblioteca SDL e orientação a objetos.
           O MINIMAX foi implementado para a inteligência artificial do jogo, bem como, o algoritmo de         
           poda alpha-beta para otimização do MINIMAX. O jogo tem suporte a partidas contra o computador 
-          e contra outro jogador humano. O jogo tem suporte a diferentes níveis de dificuldade, que são 
+          e o computador contra ele mesmo. O jogo tem suporte a diferentes níveis de dificuldade, que são 
           configurados por meio da profundidade de busca do algoritmo MINIMAX.
         </p>
       </ProjetoResumo>
